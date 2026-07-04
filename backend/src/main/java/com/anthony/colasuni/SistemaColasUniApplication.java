027aed9c58816745a3c35bd06a6d320e9def1516
toniix <anthoguerra2018@gmail.com>
2026-07-03 19:26:02 -0500

    initial commit

---
+++ backend/src/main/java/com/anthony/colasuni/SistemaColasUniApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.colasuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaColasUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaColasUniApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/anthony/colasuni/SistemaColasUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.colasuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaColasUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
